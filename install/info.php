ac9e1d5c6f41d788de5b21af84621d1419893d53
BernhardBaumrock <BernhardBaumrock@users.noreply.github.com>
2024-03-16 13:26:28 +0000

    Update main to output generated at aed00c5

--- install/info.php
+++ install/info.php
@@ -1,7 +1,10 @@
-<?php namespace ProcessWire;
-if(!defined("PROCESSWIRE_INSTALL")) die();
+<?php
+
+namespace ProcessWire;
+
+if (!defined("PROCESSWIRE_INSTALL")) die();
 $info = array(
-	'title' => "RockFrontend + UIkit + TailwindCSS", 
-	'summary' => "", 
+	'title' => "RockFrontend + UIkit + TailwindCSS",
+	'summary' => "This site profile integrates RockFrontend with UIkit and TailwindCSS for a powerful and flexible front-end development environment, providing a solid foundation for building responsive and modern web applications.",
 	'screenshot' => ""
 );
