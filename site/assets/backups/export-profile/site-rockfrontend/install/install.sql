b45afa14662eba52a37d69af907ada6e1b39a896
BernhardBaumrock <office@baumrock.com>
2024-03-25 19:43:28 +0100

    update docs for tailwind

--- site/assets/backups/export-profile/site-rockfrontend/install/install.sql
+++ site/assets/backups/export-profile/site-rockfrontend/install/install.sql
@@ -1,4 +1,4 @@
-# --- WireDatabaseBackup {"time":"2024-03-25 19:33:59","user":"","dbName":"db","description":"","tables":[],"excludeTables":["pages_drafts","pages_roles","permissions","roles","roles_permissions","users","users_roles","user","role","permission"],"excludeCreateTables":[],"excludeExportTables":["field_roles","field_permissions","field_email","field_pass","caches","session_login_throttle","page_path_history"]}
+# --- WireDatabaseBackup {"time":"2024-03-25 19:43:13","user":"","dbName":"db","description":"","tables":[],"excludeTables":["pages_drafts","pages_roles","permissions","roles","roles_permissions","users","users_roles","user","role","permission"],"excludeCreateTables":[],"excludeExportTables":["field_roles","field_permissions","field_email","field_pass","caches","session_login_throttle","page_path_history"]}
 
 DROP TABLE IF EXISTS `caches`;
 CREATE TABLE `caches` (
