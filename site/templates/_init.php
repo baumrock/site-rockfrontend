150c13fd3b323b2c932bf58124891cba802a6f8a
BernhardBaumrock <office@baumrock.com>
2024-03-25 19:11:24 +0100

    feat: improve docs and update site profile

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -9,13 +9,19 @@ namespace ProcessWire;
 $rf = rockfrontend();
 
 $rf->styles()
-  ->add('/site/templates/bundle/tailwind.css')
+  // add the base uikit theme
   ->add('/site/templates/uikit/src/less/uikit.theme.less')
+  // add default folders like /sections and /partials
   ->addDefaultFolders()
+  // add the bundled tailwind utility classes
+  ->add('/site/templates/bundle/tailwind.css')
+  // minify on production
   ->minify($config->debug ? false : true);
 
 $rf->scripts()
-  ->add('/site/templates/uikit/dist/js/uikit.min.js')
+  // load uikit
+  ->add('/site/templates/uikit/dist/js/uikit.min.js', 'defer')
+  // load custom javascript of this project
   ->add('/site/templates/scripts/main.js', 'defer')
-  ->add('/site/modules/RockCommerce/RockCommerceSite.min.js', 'defer')
+  // minify on production
   ->minify($config->debug ? false : true);
