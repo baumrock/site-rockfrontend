88d5e7bc8739b149ee7e51518f80ebb6da5d28b2
BernhardBaumrock <office@baumrock.com>
2024-07-02 14:40:39 +0200

    chore: add local config example file

---
+++ site/config-local.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ProcessWire;
+
+/** @var Config $config */
+
+$config->debug = true;
+$config->advanced = true;
+
+// database etc
+$config->dbName = 'db';
+$config->dbUser = 'db';
+$config->dbPass = 'db';
+$config->dbHost = 'db';
+$config->userAuthSalt = '12345';
+$config->tableSalt = '12345';
+$config->httpHosts = ['site-rockfrontend.ddev.site'];
+$config->sessionFingerprint = false;
+
+// RockFrontend
+$config->livereload = 1;
+$config->livereloadBuild = true;
+
+// RockMigrations
+// $config->filesOnDemand = 'https://your-live.site/';
+$config->rockmigrations = [
+  'syncSnippets' => true,
+];
+
+// tracy config for ddev development
+$config->tracy = [
+  'outputMode' => 'development',
+  'guestForceDevelopmentLocal' => true,
+  'forceIsLocal' => true,
+  'localRootPath' => getenv("TRACY_LOCALROOTPATH"),
+  'numLogEntries' => 100, // for RockMigrations
+  'editor' => 'cursor://file/%file:%line',
+];
